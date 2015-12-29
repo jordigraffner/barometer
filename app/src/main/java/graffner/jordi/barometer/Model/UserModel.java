2225ccb4153a336bfc59a4ad7f78c9bb61b1a6e6
Jordi Gräffner <s1075180@student.hsleiden.nl>
2015-12-29 11:49:03 +0100

    new commit

---
+++ app/src/main/java/graffner/jordi/barometer/Model/UserModel.java
@@ -0,0 +1,12 @@
+package graffner.jordi.barometer.Model;
+
+/**
+ * Created by jordigraffner on 29/12/15.
+ */
+public class UserModel {
+    public String name = "";
+
+    public UserModel(String name){
+        this.name = name;
+    }
+}
