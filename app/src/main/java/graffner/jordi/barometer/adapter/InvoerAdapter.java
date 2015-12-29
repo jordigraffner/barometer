656e3a62ca3a3183dff114577964d1872de7e5a3
wesleytjin <wesleytjin@msn.com>
2015-12-29 13:22:31 +0100

    adapter en row

---
+++ app/src/main/java/graffner/jordi/barometer/adapter/InvoerAdapter.java
@@ -0,0 +1,49 @@
+package graffner.jordi.barometer.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import graffner.jordi.barometer.Model.CourseModel;
+import graffner.jordi.barometer.R;
+
+/**
+ * Created by wesley tjin on 29-12-15.
+ */
+public class InvoerAdapter extends ArrayAdapter<CourseModel> {
+
+public InvoerAdapter(Context context, int resource, List<CourseModel> objects){
+        super(context, resource, objects);
+        }
+
+@Override
+public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder vh;
+
+        if (convertView == null ) {
+        vh = new ViewHolder();
+        LayoutInflater li = LayoutInflater.from(getContext());
+        convertView = li.inflate(R.layout.view_content_row, parent, false);
+        vh.name = (TextView) convertView.findViewById(R.id.subject_name);
+        vh.code = (TextView) convertView.findViewById(R.id.subject_code);
+        convertView.setTag(vh);
+        } else {
+        vh = (ViewHolder) convertView.getTag();
+        }
+        CourseModel cm = getItem(position);
+        vh.name.setText(cm.name);
+        vh.name.setText(cm.ects);
+        return convertView;
+        }
+
+private static class ViewHolder {
+    TextView name;
+    TextView code;
+
+}
+}
