b85c666dda9a218eff793f28a0de134aa8137950
Jordi Gräffner <s1075180@student.hsleiden.nl>
2016-01-05 15:37:38 +0100

    Cirkeldiagram

--- app/src/main/java/graffner/jordi/barometer/OverzichtActivity.java
+++ app/src/main/java/graffner/jordi/barometer/OverzichtActivity.java
@@ -19,11 +19,14 @@ public class OverzichtActivity extends AppCompatActivity {
     private PieChart mChart;
     public static final int MAX_ECTS = 60;
     public static int currentEcts = 0;
+    public static int unknowntEcts = 60;
+    public static int failedEcts = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overzicht);
+
         mChart = (PieChart) findViewById(R.id.chart);
         mChart.setDescription("");
         mChart.setTouchEnabled(false);
@@ -32,50 +35,55 @@ public class OverzichtActivity extends AppCompatActivity {
         mChart.setTransparentCircleColor(Color.rgb(130, 130, 130));
         mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
 
-
         setData(0);
-
-        Button fab = (Button) findViewById(R.id.plusTweeTest);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (currentEcts < MAX_ECTS) {
-                    setData(currentEcts += 2);
-                } else {
-                    setData(currentEcts = 0);
-                }
-            }
-        });
-
     }
 
     private void setData(int aantal) {
         currentEcts = aantal;
+
+        Double[] lijst = new Double[3];
+        lijst[0] = 5.0;
+        lijst[1] = 6.6;
+        lijst[2] = 0.0;
+
         ArrayList<Entry> yValues = new ArrayList<>();
         ArrayList<String> xValues = new ArrayList<>();
 
-        yValues.add(new Entry(60 - currentEcts, 1));
+        for(Double getal: lijst){
+            if(getal > 5.4){
+                currentEcts += 3;
+                unknowntEcts -= 3;
+            } else {
+                failedEcts += 3;
+                unknowntEcts -= 3;
+            }
+        }
+
+        yValues.add(new Entry(unknowntEcts, 0));
         xValues.add("Resterend ECTS");
 
-        yValues.add(new Entry(aantal, 0));
+        yValues.add(new Entry(currentEcts, 1));
         xValues.add("Behaalde ECTS");
 
-        yValues.add(new Entry(60 - currentEcts, 1));
-        xValues.add("Niet Behaald ECTS");
+        yValues.add(new Entry(failedEcts, 2));
+        xValues.add("Niet behaald");
+
+//        yValues.add(new Entry(60 - currentEcts, 1));
+//        xValues.add("Resterend ECTS");
+//
+//        yValues.add(new Entry(aantal, 0));
+//        xValues.add("Behaalde ECTS");
+//
+//        yValues.add(new Entry(60 - currentEcts, 1));
+//        xValues.add("Niet Behaald ECTS");
 
 
 
         //  http://www.materialui.co/colors
         ArrayList<Integer> colors = new ArrayList<>();
-        if (currentEcts <60) {
-            colors.add(Color.rgb(51,255,0));
-        } else if (currentEcts < 40){
-            colors.add(Color.rgb(235,0,0));
-        } else if  (currentEcts < 50) {
-            colors.add(Color.rgb(253,216,53));
-        } else {
-            colors.add(Color.rgb(67,160,71));
-        }
+       colors.add(Color.rgb(255,255,0));
+
+        colors.add(Color.rgb(0,255,0));
 
         colors.add(Color.rgb(255,0,0));
 
