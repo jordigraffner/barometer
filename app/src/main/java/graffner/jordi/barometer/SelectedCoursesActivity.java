923ed9c5aa4ef89540cac69b4b4661dfd7fbe215
Jordi Gräffner <s1075180@student.hsleiden.nl>
2016-01-15 16:33:11 +0100

    commit files

---
+++ app/src/main/java/graffner/jordi/barometer/SelectedCoursesActivity.java
@@ -0,0 +1,69 @@
+package graffner.jordi.barometer;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import graffner.jordi.barometer.Model.CourseModel;
+import graffner.jordi.barometer.adapter.InvoerAdapter;
+import graffner.jordi.barometer.database.DatabaseHelper;
+import graffner.jordi.barometer.database.DatabaseInfo;
+
+public class SelectedCoursesActivity extends AppCompatActivity {
+
+    private int compareGradeStart;
+    private int compareGradeEnd;
+    private List<CourseModel> courseModels = new ArrayList<>();
+    private List<CourseModel> showCourses = new ArrayList<>();
+    private  DatabaseHelper dbHelper = DatabaseHelper.getHelper(this);
+    private InvoerAdapter mAdapter;
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_courses);
+
+        Intent myIntent = getIntent();
+
+        final String valueName = myIntent.getStringExtra("key"); // will return "FirstKeyValue"
+        if(valueName.equals("Resterend ECTS")){
+            compareGradeStart = 0;
+            compareGradeEnd = 0;
+        } else if(valueName.equals(("Behaalde ECTS"))){
+            compareGradeStart = 6;
+            compareGradeEnd = 10;
+        } else{
+            compareGradeStart = 1;
+            compareGradeEnd = 5;
+        }
+        loadCourses();
+        for(CourseModel course: courseModels){
+            if(Double.parseDouble(course.grade) >= compareGradeStart && Double.parseDouble(course.grade) <= compareGradeEnd){
+                showCourses.add(course);
+            }
+        }
+
+        mListView = (ListView) findViewById(R.id.courses_list);
+        mAdapter = new InvoerAdapter(SelectedCoursesActivity.this, 0, showCourses);
+        mListView.setAdapter(mAdapter);
+    }
+
+    public void loadCourses(){
+        Cursor res = dbHelper.query(DatabaseInfo.BarometerTables.COURSE, new String[]{"*"}, null, null, null, null, null);
+        //Laad courses in list
+        res.moveToFirst();
+        while(res.moveToNext()){
+            courseModels.add(new CourseModel(res.getString(res.getColumnIndex("name")),res.getString(res.getColumnIndex("ects")), res.getString(res.getColumnIndex("grade")), res.getString(res.getColumnIndex("period"))));
+        }
+    }
+}
